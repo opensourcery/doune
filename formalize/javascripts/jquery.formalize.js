4621f8424c6550bf5fd05942daf874cc71a29a45
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-05-27 14:32:12 -0700

    Update formalize.

--- formalize/javascripts/jquery.formalize.js
+++ formalize/javascripts/jquery.formalize.js
@@ -1,1 +1,150 @@
-var FORMALIZE=(function(b,e,f,a){var h="placeholder" in f.createElement("input");var i="autofocus" in f.createElement("input");var g="webkitAppearance" in f.createElement("select").style;var d=!!(b.browser.msie&&parseInt(b.browser.version,10)===6);var c=!!(b.browser.msie&&parseInt(b.browser.version,10)===7);return{go:function(){for(var j in FORMALIZE.init){FORMALIZE.init[j]();}},init:{detect_webkit:function(){if(!g){return;}b("html").addClass("is_webkit");},full_input_size:function(){if(!c||!b("textarea, input.input_full").length){return;}b("textarea, input.input_full").wrap('<span class="input_full_wrap"></span>');},ie6_skin_inputs:function(){if(!d||!b("input, select, textarea").length){return;}var j=/button|submit|reset/;var k=/date|datetime|datetime-local|email|month|number|password|range|search|tel|text|time|url|week/;b("input").each(function(){var l=b(this);if(this.getAttribute("type").match(j)){l.addClass("ie6_button");if(this.disabled){l.addClass("ie6_button_disabled");}}else{if(this.getAttribute("type").match(k)){l.addClass("ie6_input");if(this.disabled){l.addClass("ie6_input_disabled");}}}});b("textarea, select").each(function(){if(this.disabled){b(this).addClass("ie6_input_disabled");}});},autofocus:function(){if(i||!b(":input[autofocus]").length){return;}b(":input[autofocus]:visible:first").focus();},placeholder:function(){if(h||!b(":input[placeholder]").length){return;}FORMALIZE.misc.add_placeholder();b(":input[placeholder]").each(function(){var j=b(this);var k=j.attr("placeholder");j.focus(function(){if(j.val()===k){j.val("").removeClass("placeholder_text");}}).blur(function(){FORMALIZE.misc.add_placeholder();});j.closest("form").submit(function(){if(j.val()===k){j.val("").removeClass("placeholder_text");}}).bind("reset",function(){setTimeout(FORMALIZE.misc.add_placeholder,50);});});}},misc:{add_placeholder:function(){if(h||!b(":input[placeholder]").length){return;}b(":input[placeholder]").each(function(){var j=b(this);var k=j.attr("placeholder");if(!j.val()||j.val()===k){j.val(k).addClass("placeholder_text");}});}}};})(jQuery,this,this.document);jQuery(document).ready(function(){FORMALIZE.go();});
+/*
+  Formalize - version 1.1
+
+  Note: This file depends on the jQuery library.
+*/
+
+// Module pattern:
+// http://yuiblog.com/blog/2007/06/12/module-pattern
+var FORMALIZE = (function($, window, document, undefined) {
+  // Private constants.
+  var PLACEHOLDER_SUPPORTED = 'placeholder' in document.createElement('input');
+  var AUTOFOCUS_SUPPORTED = 'autofocus' in document.createElement('input');
+  var IE6 = !!($.browser.msie && parseInt($.browser.version, 10) === 6);
+  var IE7 = !!($.browser.msie && parseInt($.browser.version, 10) === 7);
+
+  // Expose innards of FORMALIZE.
+  return {
+    // FORMALIZE.go
+    go: function() {
+      for (var i in FORMALIZE.init) {
+        FORMALIZE.init[i]();
+      }
+    },
+    // FORMALIZE.init
+    init: {
+      // FORMALIZE.init.full_input_size
+      full_input_size: function() {
+        if (!IE7 || !$('textarea, input.input_full').length) {
+          return;
+        }
+
+        // This fixes width: 100% on <textarea> and class="input_full".
+        // It ensures that form elements don't go wider than container.
+        $('textarea, input.input_full').wrap('<span class="input_full_wrap"></span>');
+      },
+      // FORMALIZE.init.ie6_skin_inputs
+      ie6_skin_inputs: function() {
+        // Test for Internet Explorer 6.
+        if (!IE6 || !$('input, select, textarea').length) {
+          // Exit if the browser is not IE6,
+          // or if no form elements exist.
+          return;
+        }
+
+        // For <input type="submit" />, etc.
+        var button_regex = /button|submit|reset/;
+
+        // For <input type="text" />, etc.
+        var type_regex = /date|datetime|datetime-local|email|month|number|password|range|search|tel|text|time|url|week/;
+
+        $('input').each(function() {
+          var el = $(this);
+
+          // Is it a button?
+          if (this.getAttribute('type').match(button_regex)) {
+            el.addClass('ie6_button');
+
+            /* Is it disabled? */
+            if (this.disabled) {
+              el.addClass('ie6_button_disabled');
+            }
+          }
+          // Or is it a textual input?
+          else if (this.getAttribute('type').match(type_regex)) {
+            el.addClass('ie6_input');
+
+            /* Is it disabled? */
+            if (this.disabled) {
+              el.addClass('ie6_input_disabled');
+            }
+          }
+        });
+
+        $('textarea, select').each(function() {
+          /* Is it disabled? */
+          if (this.disabled) {
+            $(this).addClass('ie6_input_disabled');
+          }
+        });
+      },
+      // FORMALIZE.init.autofocus
+      autofocus: function() {
+        if (AUTOFOCUS_SUPPORTED || !$(':input[autofocus]').length) {
+          return;
+        }
+
+        $(':input[autofocus]:visible:first').focus();
+      },
+      // FORMALIZE.init.placeholder
+      placeholder: function() {
+        if (PLACEHOLDER_SUPPORTED || !$(':input[placeholder]').length) {
+          // Exit if placeholder is supported natively,
+          // or if page does not have any placeholder.
+          return;
+        }
+
+        FORMALIZE.misc.add_placeholder();
+
+        $(':input[placeholder]').each(function() {
+          var el = $(this);
+          var text = el.attr('placeholder');
+
+          el.focus(function() {
+            if (el.val() === text) {
+              el.val('').removeClass('placeholder_text');
+            }
+          }).blur(function() {
+            FORMALIZE.misc.add_placeholder();
+          });
+
+          // Prevent <form> from accidentally
+          // submitting the placeholder text.
+          el.closest('form').submit(function() {
+            if (el.val() === text) {
+              el.val('').removeClass('placeholder_text');
+            }
+          }).bind('reset', function() {
+            setTimeout(FORMALIZE.misc.add_placeholder, 50);
+          });
+        });
+      }
+    },
+    // FORMALIZE.misc
+    misc: {
+      // FORMALIZE.misc.add_placeholder
+      add_placeholder: function() {
+        if (PLACEHOLDER_SUPPORTED || !$(':input[placeholder]').length) {
+          // Exit if placeholder is supported natively,
+          // or if page does not have any placeholder.
+          return;
+        }
+
+        $(':input[placeholder]').each(function() {
+          var el = $(this);
+          var text = el.attr('placeholder');
+
+          if (!el.val() || el.val() === text) {
+            el.val(text).addClass('placeholder_text');
+          }
+        });
+      }
+    }
+  };
+// Alias jQuery, window, document.
+})(jQuery, this, this.document);
+
+// Automatically calls all functions in FORMALIZE.init
+jQuery(document).ready(function() {
+  FORMALIZE.go();
+});
