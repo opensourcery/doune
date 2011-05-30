be44a97c10f56d168980409bc3fdbcdd28edf56a
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-05-29 19:01:02 -0700

    Use theme-relative paths for when Sass wants to 'fix' paths.

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
-http_path = "/profiles/PROJECT/themes/doune/"
+http_path = "."
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
