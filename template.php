e6f4ff921ff935c7dbc3d9d2bab65d3371c0674a
Adam J. DiCarlo <adam.dicarlo@gmail.com>
2011-06-09 11:17:50 -0700

    Pager theming -- adapted from Tao.

--- template.php
+++ template.php
@@ -5,6 +5,24 @@
  */
 
 /**
+ * Implements hook_theme().
+ */
+function doune_theme() {
+  $items = array();
+
+  // Split out pager list into separate theme function.
+  $items['pager_list'] = array('arguments' => array(
+    'tags' => array(),
+    'limit' => 10,
+    'element' => 0,
+    'parameters' => array(),
+    'quantity' => 9,
+  ));
+
+  return $items;
+}
+
+/**
  * Implements hook_css_alter(). Borrowed from Tao.
  *
  * Omitted:
@@ -185,3 +203,171 @@ function doune_preprocess_search_block_form(&$vars) {
   $vars['search_form'] = str_replace('value=""', 'placeholder="Search"', $vars['search_form']);
 }
 
+/**
+ * Override of theme_pager(). Adapted from Tao.
+ */
+function doune_pager($vars) {
+  $tags = $vars['tags'];
+  $element = $vars['element'];
+  $parameters = $vars['parameters'];
+  $quantity = $vars['quantity'];
+  $vars['unthemed'] = TRUE;
+  $pager_links = theme('pager_list', $vars);
+
+  $links = array();
+  $links['pager-first'] = theme('pager_first', array(
+    'text' => (isset($tags[0]) ? $tags[0] : t('First')),
+    'element' => $element,
+    'parameters' => $parameters
+  ));
+  $links['pager-previous'] = theme('pager_previous', array(
+    'text' => (isset($tags[1]) ? $tags[1] : t('Prev')),
+    'element' => $element,
+    'interval' => 1,
+    'parameters' => $parameters
+  ));
+  $links = array_merge($links, $pager_links);
+  $links['pager-next'] = theme('pager_next', array(
+    'text' => (isset($tags[3]) ? $tags[3] : t('Next')),
+    'element' => $element,
+    'interval' => 1,
+    'parameters' => $parameters
+  ));
+  $links['pager-last'] = theme('pager_last', array(
+    'text' => (isset($tags[4]) ? $tags[4] : t('Last')),
+    'element' => $element,
+    'parameters' => $parameters
+  ));
+  $links = array_filter($links);
+  $pager_list = theme('links', array(
+    'links' => $links,
+    'attributes' => array('class' => 'links pager pager-links')
+  ));
+  if ($pager_list) {
+    return "<div class='pager clearfix'>$pager_list</div>";
+  }
+}
+
+/**
+ * Split out page list generation into its own function.
+ */
+function doune_pager_list($vars) {
+  $tags = $vars['tags'];
+  $element = $vars['element'];
+  $parameters = $vars['parameters'];
+  $quantity = $vars['quantity'];
+
+  global $pager_page_array, $pager_total;
+  if ($pager_total[$element] > 1) {
+    // Calculate various markers within this pager piece:
+    // Middle is used to "center" pages around the current page.
+    $pager_middle = ceil($quantity / 2);
+    // current is the page we are currently paged to
+    $pager_current = $pager_page_array[$element] + 1;
+    // first is the first page listed by this pager piece (re quantity)
+    $pager_first = $pager_current - $pager_middle + 1;
+    // last is the last page listed by this pager piece (re quantity)
+    $pager_last = $pager_current + $quantity - $pager_middle;
+    // max is the maximum page number
+    $pager_max = $pager_total[$element];
+    // End of marker calculations.
+
+    // Prepare for generation loop.
+    $i = $pager_first;
+    if ($pager_last > $pager_max) {
+      // Adjust "center" if at end of query.
+      $i = $i + ($pager_max - $pager_last);
+      $pager_last = $pager_max;
+    }
+    if ($i <= 0) {
+      // Adjust "center" if at start of query.
+      $pager_last = $pager_last + (1 - $i);
+      $i = 1;
+    }
+    // End of generation loop preparation.
+
+    $links = array();
+
+    // When there is more than one page, create the pager list.
+    if ($i != $pager_max) {
+      // Now generate the actual pager piece.
+      for ($i; $i <= $pager_last && $i <= $pager_max; $i++) {
+        if ($i < $pager_current) {
+          $links["pager-item-$i pager-item"] = theme('pager_previous', array(
+            'text' => $i,
+            'element' => $element,
+            'interval' => ($pager_current - $i),
+            'parameters' => $parameters
+          ));
+        }
+        if ($i == $pager_current) {
+          $links["pager-item-$i pager-current"] = array('title' => $i);
+        }
+        if ($i > $pager_current) {
+          $links["pager-item-$i pager-item"] = theme('pager_next', array(
+            'text' => $i,
+            'element' => $element,
+            'interval' => ($i - $pager_current),
+            'parameters' => $parameters
+          ));
+        }
+      }
+      return isset($vars['unthemed']) ? $links :
+        theme('links', array(
+          'links' => $links,
+          'attributes' => array('class' => 'links pager pager-list')
+        ));
+    }
+  }
+  return isset($vars['unthemed']) ? array() : '';
+}
+
+/**
+ * Return an array suitable for theme_links() rather than marked up HTML link.
+ */
+function doune_pager_link($vars) {
+  $text = $vars['text'];
+  $page_new = $vars['page_new'];
+  $element = $vars['element'];
+  $parameters = $vars['parameters'];
+  $attributes = $vars['attributes'];
+
+  $page = isset($_GET['page']) ? $_GET['page'] : '';
+  if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
+    $parameters['page'] = $new_page;
+  }
+
+  $query = array();
+  if (count($parameters)) {
+    $query = drupal_get_query_parameters($parameters, array());
+  }
+  if ($query_pager = pager_get_query_parameters()) {
+    $query = array_merge($query, $query_pager);
+  }
+
+  // Set each pager link title
+  if (!isset($attributes['title'])) {
+    static $titles = NULL;
+    if (!isset($titles)) {
+      $titles = array(
+        t('« first') => t('Go to first page'),
+        t('‹ previous') => t('Go to previous page'),
+        t('next ›') => t('Go to next page'),
+        t('last »') => t('Go to last page'),
+      );
+    }
+    if (isset($titles[$text])) {
+      $attributes['title'] = $titles[$text];
+    }
+    else if (is_numeric($text)) {
+      $attributes['title'] = t('Go to page @number', array('@number' => $text));
+    }
+  }
+
+  return array(
+    'title' => $text,
+    'href' => $_GET['q'],
+    'attributes' => $attributes,
+    'query' => count($query) ? $query : NULL,
+  );
+}
